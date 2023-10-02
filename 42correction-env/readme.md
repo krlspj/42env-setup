22ab8761fdd083a7bf4b50d618a6082a02ee80f3
krlspj <krls.pjds@gmail.com>
2023-10-03 01:35:35 +0200

    download nvim

--- 42correction-env/readme.md
+++ 42correction-env/readme.md
@@ -1,3 +1,7 @@
 # 42correction-env
 Are you tired of dealing with subpar Vim configurations from other users?  
-I'm working on simplifying the process of setting up my Neovim configuration on campus computers and providing a way to remove it completely without leaving a trace.
+I'm working on simplifying the process of setting up my Neovim configuration on campus computers and providing a way to remove it completely without leaving a trace.  
+**To run the script**
+```
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/krlspj/42env-setup/main/42correction-env/setup_neovim.sh)"
+```
--- 42correction-env/setup_neovim.sh
+++ 42correction-env/setup_neovim.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 echo "check if neovim exists"
 echo "install neovim for mac"
-# if mac: https://github.com/neovim/neovim/releases/latest/download/nvim-macos.tar.gz
-# if linux: https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+MAC_LINK=https://github.com/neovim/neovim/releases/latest/download/nvim-macos.tar.gz
+LINUX_LINK=https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+
+#wget -P /tmp $MAC_LINK
+#tar -xf /tmp/nvim-macos.tar.gz -C /tmp
+#wget -P /tmp $LINUX_LINK
+tar -xf /tmp/nvim-linux64.tar.gz -C /tmp
+
+# # required
+#mv ~/.config/nvim{,.bak}
+# optional but recommended
+#mv ~/.local/share/nvim{,.bak}
+#mv ~/.local/state/nvim{,.bak}
+#mv ~/.cache/nvim{,.bak}
 echo "configure neovim"
 echo "COMMAND to remove neovim and its packages"
