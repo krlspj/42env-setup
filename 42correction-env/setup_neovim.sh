01fbc383d750fe557e5983f75308b73d1c796120
krlspj <43220110+krlspj@users.noreply.github.com>
2023-10-03 00:04:03 +0200

    mac and linux neovim download links

--- 42correction-env/setup_neovim.sh
+++ 42correction-env/setup_neovim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "check if neovim exists"
 echo "install neovim for mac"
+# if mac: https://github.com/neovim/neovim/releases/latest/download/nvim-macos.tar.gz
+# if linux: https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 echo "configure neovim"
 echo "COMMAND to remove neovim and its packages"
