1ea1f87465d5be7812ab00bf57bfcea956e97f9a
krlspj <43220110+krlspj@users.noreply.github.com>
2023-10-04 19:10:19 +0200

    clean files when done

--- 42correction-env/setup_neovim.sh
+++ 42correction-env/setup_neovim.sh
@@ -8,12 +8,20 @@ LINUX_LINK=https://github.com/neovim/neovim/releases/latest/download/nvim-linux6
 #tar -xf /tmp/nvim-macos.tar.gz -C /tmp
 #wget -P /tmp $LINUX_LINK
 tar -xf /tmp/nvim-linux64.tar.gz -C /tmp
-
-# # required
+# check if path exists. if does, stop process since there can be an already configured neovim
 #mv ~/.config/nvim{,.bak}
 # optional but recommended
 #mv ~/.local/share/nvim{,.bak}
 #mv ~/.local/state/nvim{,.bak}
 #mv ~/.cache/nvim{,.bak}
+#clean nvim
+
+# # required
+#rm ~/.config/nvim{,.bak}
+# optional but recommended
+#rm ~/.local/share/nvim{,.bak}
+#rm ~/.local/state/nvim{,.bak}
+#rm ~/.cache/nvim{,.bak}
+#rm -rf ~/.config/nvim/.git
 echo "configure neovim"
 echo "COMMAND to remove neovim and its packages"
